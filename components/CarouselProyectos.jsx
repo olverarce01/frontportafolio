84398c89c1902c8ac34655ff5c214f7c82c01931
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-06-10 11:46:48 -0400

    no like

--- components/CarouselProyectos.jsx
+++ components/CarouselProyectos.jsx
@@ -1,54 +1,24 @@
 'use client'
 
-import { useEffect } from 'react';
 import { Carousel, Modal, Button } from 'react-bootstrap';
 import styles from './CarouselProyectos.module.css';
 import { useState } from 'react';
-import { FaThumbsUp} from 'react-icons/fa';
-import host from '@/host';
 
 export default function CarouselProyectos({images}) { 
 
 
   const [showModal, setShowModal] = useState(false);
   const [modalImage, setModalImage] = useState('');
-  const [idImage,setIdImage] = useState('');
-  const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState(0);
 
 
-  const fetchLikes = (id) => {
-    return fetch(`${host}/images/${id}`, {cache: 'no-store'})
-    .then(res=>res.json());
-  }
-  const fetchStatusLikeImage = (id) => {
-    return fetch(`${host}/images/${id}/like`, {cache: 'no-store'})
-    .then(res=>res.json());
-  }
 
   const handleImageClick = async (src,id) => {
     setModalImage(src);
-    setIdImage(id);
     setShowModal(true);
-    setLikes(await fetchLikes(id))
-    setIsLiked(await fetchStatusLikeImage(id))
 
-  };
-  
+  };  
   const handleCloseModal = () => {
     setShowModal(false);
-  };
-  const handleLike = async() => {
-    setIsLiked(!isLiked);
-    await fetch(`${host}/images/${idImage}/like`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({id: idImage}),
-    })
-    setLikes(await fetchLikes(idImage))
-    
   };
   return (
     <div className={`col-md-7 ${styles.col}`}>
@@ -68,12 +38,6 @@ export default function CarouselProyectos({images}) {
       <Modal.Body className={styles.zoomable}>
         <img src={modalImage} alt="Imagen ampliada" className="img-fluid" style={{width: '100%', height: 'auto'}}/>
       </Modal.Body>
-      <Modal.Footer>
-          <Button variant={isLiked ? "white": "primary"} onClick={handleLike}>
-           <FaThumbsUp /> {isLiked ? "No me gusta": "Me gusta"}
-          </Button>
-          {likes} Me gusta
-        </Modal.Footer>
     </Modal>
     </div>
   )
